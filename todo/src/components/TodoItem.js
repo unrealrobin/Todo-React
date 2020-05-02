493a6e10b5e522fd5ff170ab2968257b289f7186
robinnnnnn <rlifshitz19@gmail.com>
2020-05-02 14:24:02 -0700

    Functional, Needs real DB

--- todo/src/components/TodoItem.js
+++ todo/src/components/TodoItem.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import moment from 'moment';
 
-let duration;
 const TodoItem = (props) => {
   const handleSetUrgency = (needDuration) => {
     let dueDate = moment(props.date);
     let todaysDate = moment();
-    duration = Math.ceil(dueDate.diff(todaysDate, 'days', true));
+    let duration = Math.ceil(dueDate.diff(todaysDate, 'days', true));
     let urgency;
     if (duration >= 7) {
       urgency = 'low-urgency';
@@ -24,12 +23,12 @@ const TodoItem = (props) => {
   setInterval(handleSetUrgency(), 1000 * 60 * 60 * 12);
 
   const removeTaskStyle = (e) => {
-    e.target.classList.add('removeClassStyle');
-    e.target.textContent = 'complete';
+    e.target.classList.add('removeClassStyle', 'material-icons');
+    e.target.textContent = 'done';
   };
 
   const implementTaskStyle = (e) => {
-    e.target.classList.remove('removeClassStyle');
+    e.target.classList.remove('removeClassStyle', 'material-icons');
     e.target.textContent = handleSetUrgency(true);
   };
 
@@ -47,7 +46,7 @@ const TodoItem = (props) => {
           props.remove(props.name);
         }}
       >
-        <h3>{duration}</h3>
+        <h3>{handleSetUrgency(true)}</h3>
       </div>
     </div>
   );
